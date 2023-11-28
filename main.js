31401a0653698366403287cbd909e76577fd7316
CamilaVerso <camilaverso2@gmail.com>
2023-11-28 13:32:59 -0300

    Criei a função que possibilita listar todas as tarefas adicionadas

--- main.js
+++ main.js
@@ -2,20 +2,30 @@
 let tarefas = [];
 
 function adicionarTarefa(mensagem) {
-tarefas.push(mensagem);
+	tarefas.push(mensagem);
 }
 
 adicionarTarefa("Limpar a casa");
 adicionarTarefa("Limpar o quarto");
 adicionarTarefa("Lavar a louça");
 
-console.log(tarefas)
+// console.log(tarefas)
 
-
-function editarTarefa (index, novaMensagem){
+// Editar a tarefa a partir do index
+function editarTarefa(index, novaMensagem) {
 	tarefas[index] = novaMensagem
 }
 
-editarTarefa(0, 'varrer o chão');
+// editarTarefa(0, 'varrer o chão');
 
-console.log(tarefas)
+// console.log(tarefas)
+
+// função que listará todas as tarefas adicionadas
+function listarTarefas() {
+	tarefas.forEach((tarefas) => {
+		console.log(`Aqui está a sua tarefa: ${tarefas}`)
+	})
+		
+}
+
+listarTarefas();
